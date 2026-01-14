ef3b775a17331d419a84c6a5343bf0e46d002be2
Bam-Manu-182 <bammanuel182@gmail.com>
2026-01-14 18:07:22 -0500

    Proyect V1.5
    Pagina 6 = Creación juego Par o Impar status = ok

---
+++ brain_games/brain_games_par_impar.py
@@ -0,0 +1,35 @@
+from brain_games.cli import welcome_user
+import random
+
+
+def is_even(number):
+    name = welcome_user()
+    
+    print('Answer "even" if the number is even, otherwise answer "odd".')
+
+    correct_followed = 0
+
+    while correct_followed < 3:
+        number = random.randint(1, 100)
+    
+        print(f'Question: {number}')
+
+        answer = input('Your answer: ').strip().lower()
+
+        if number % 2 == 0:
+            correct_answer = 'even'
+        else:
+            correct_answer = 'odd'
+
+        if answer == correct_answer:
+            print('Correct!')
+            correct_followed += 1
+        else:
+            print(f"'{answer}' is wrong answer ;(. "
+                  f"Correct answer was '{correct_answer}'.")
+            print(f"Let's try again, {name}!")
+            return
+    print(f'Congratulations, {name}!')
+
+if __name__ == "__main__":
+    is_even(None)
