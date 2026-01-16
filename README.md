04de5d861f6574e467b3be21887e1b32f29d8065
Bam-Manu-182 <bammanuel182@gmail.com>
2026-01-15 19:23:40 -0500

    Proyect V1.8
    Realización pagina 9. Creación del Juego de Progression. Se realizan pruebas y se sube el video de demostración.
    Status = OK

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 [![Maintainability](https://qlty.sh/gh/Bam-Manu-182/projects/python-project-140/maintainability.svg)](https://qlty.sh/gh/Bam-Manu-182/projects/python-project-140)
 Demostracion Juego Calculadora. https://drive.google.com/file/d/1Y5wSL55HxgJnCddHTFriRS0i0C5o98iv/view?usp=drive_link
 Demostracion Juego MCD. https://drive.google.com/file/d/1wbjrqRBTFob0eM4ufRpA3LnTPxGIX3Qr/view?usp=drive_link
+Demostracion Juego Progression. https://drive.google.com/file/d/1KnE65mIuU_6dG1Qye5DfPpfCtdbLSDlP/view?usp=drive_link
+Demostracion Juego Par o Impar. La respuesta a este juego es decir yes = es par. O no = no es impar. https://drive.google.com/file/d/1CHFvPHn9_RjK-yzZr1Rga1YuFWbZCnRn/view?usp=drive_link
---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,51 @@
+from brain_games.cli import welcome_user
+import random
+
+
+def generate_progression():
+    length = random.randint(5, 10)
+    start = random.randint(1, 20)
+    step = random.randint(1, 10)
+
+    progression = []
+
+    for i in range(length):
+        progression.append(str(start + i * step))
+
+    hidden_index = random.randint(0, length - 1)
+
+    correct_answer = progression[hidden_index]
+    progression[hidden_index] = '..'
+
+    return ' '.join(progression), correct_answer
+
+
+def play_progression():
+    name = welcome_user()
+
+    print("What number is missing in the progression?")
+
+    rounds_to_win = 3
+
+    for _ in range(rounds_to_win):
+        progression, correct_answer = generate_progression()
+
+        print(f"Question: {progression}")
+
+        user_answer = input("Your answer: ")
+
+        if user_answer == correct_answer:
+            print("Correct!")
+
+        else:
+            print(f"'{user_answer}' is wrong answer ;(. "
+                  f"Correct answer was '{correct_answer}'.")
+            print(f"Let's try again, {name}!")
+
+            return
+
+    print(f"Congratulations, {name}!")
+
+
+if __name__ == "__main__":
+    play_progression()
